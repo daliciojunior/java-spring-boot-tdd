768dbd170302f36a48bf4adf187bdf8598da427e
daliciojunior <daliciojunior@gmail.com>
2022-08-30 07:09:22 -0300

    Spring Boot starter files set up

---
+++ src/main/java/com/daliciojunior/javaspringboottdd/JavaSpringBootTddApplication.java
@@ -0,0 +1,13 @@
+package com.daliciojunior.javaspringboottdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringBootTddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringBootTddApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/daliciojunior/javaspringboottdd/JavaSpringBootTddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daliciojunior.javaspringboottdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringBootTddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
